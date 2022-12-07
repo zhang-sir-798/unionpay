1c2ada45f841dabe0502db9ff3b18e943230bf8e
zhang_sir <352521799@163.com>
2022-12-07 14:31:51 +0800

    Update demo.java
    55

--- unionpay/src/main/java/com/dx/service/demo.java
+++ unionpay/src/main/java/com/dx/service/demo.java
@@ -15,9 +15,9 @@ import java.util.Set;
 @SuppressWarnings({ "rawtypes", "unchecked" })
 public class demo {
 
-	public static String url = "https://smbp.95516.com/xwins/gateway/batchGenerateQrcode";
-	protected static final String aesKey = "U6tIyxMKfcLcg+trazjphA==";
-	protected static final String rsaKey = "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAKHDyGodZlZdeIiSHBFpkXuDX0xqPao+TrlcIKKQIEJfXN8nlKkHZbHlsB39ZpGMRqwfutrYH1vXpk6beU2y6ceWaIGCBlrU+kUQqWQywXzBU+xdtzEsTvMYw7CNslWtW/UyAKC2wCF5B16lUxfGVhMlKMyi5ltK2UfoxAyeBtUxAgMBAAECgYA3PZB3DEsiOxEqMhr7pz1THG2yvqUIOfbQPbG3UdhyUfZ5Dh8qCaiks3cNBre8BFct+Dfq+9rZli7osRcffXVvcmWTVoN3VaLtPq7QJ621yc+N1pyUUMx9eyExdVdqK6WbaDmxvMjpBP8FtzuMD9a8AALgI3SGwZ8F0k5dyr7rQQJBAOul9YdaAi3C6eY4/ik9H+tx/k/PXYRCNKbQlfCbBw2ybGB+RaMfUpbMAkHBS1PvWpqPMRSM1CgEVe3w2EBWdK0CQQCvvE3jcXvq9CeU0a/y8DyJr1Qnrw6Sw7FRV2R6rFZSP0GOgtYs5oSxXWreFlv83r0KLZ+xb7GeNowdF+16ga8VAkB2FMtb77Z3x9v9CUIS2BZOnOiahYaO574KeSspTgXSzz2PfLbYbrGr7r6SxJFClgYIvQgTDB6jjsigWreCRdBhAkALcytn/Ebkog/KJHdgTsLC/cdv5nn+mf19nUIQtBXTugddb6zgtAxFOh7yZWaM7T5H4X0cJHjgJBGw+5QffT+ZAkApRQccz6N/0BNR7tYPzM6cnJHviF1O1uJGSBafTpDA97LeDEo3BQNpqt4mmmRNVO3yxZBB6jB5CQaL6Japv2WH";
+	public static String url = "https://127.0.0.1";
+	protected static final String aesKey = "1111";
+	protected static final String rsaKey = "1111";
 
 	// protected static String rsaKey = ApiUtil.getPublicKeyFromPem();
 
@@ -113,7 +113,7 @@ public class demo {
 		bizMap.put("merName", mer.getMer_name());
 		bizMap.put("certifTp", "01");
 		//需要修改为活参
-		bizMap.put("qrCode", "https://qr.95516.com/00010002/01182341505319001023559100830252");
+		bizMap.put("qrCode", "https://127.0.0.1");
 		
 		bizMap.put("certifId", mer.getLegal_id());
 		bizMap.put("customerNm", mer.getLegal_name());
@@ -135,44 +135,44 @@ public class demo {
 
 	// 批量查询
 	protected static void batchQrcodeQuery(Map<String, Object> bizMap) {
-		bizMap.put("batchNo", "00000317");
+		bizMap.put("batchNo", "11");
 	}
 
 	// 二维码查询
 	protected static void queryQrcode(Map<String, Object> bizMap) {
-		bizMap.put("qrCode", "https://qr.95516.com/00010002/62422309557386655861967507435823");
+		bizMap.put("qrCode", "https://127.0.0.1");
 	}
 
 	// 4.2.17收款码名称修改
 	protected static void modifyQrcode(Map<String, Object> bizMap) {
-		bizMap.put("merId", "QRC330100000274");
-		bizMap.put("qrNum", "483600006100000000");
-		bizMap.put("qrCodeName", "粉色发设");
+		bizMap.put("merId", "11");
+		bizMap.put("qrNum", "11");
+		bizMap.put("qrCodeName", "11");
 	}
 
 	// 4.2.15一户多码申请
 	protected static void multiQrcode(Map<String, Object> bizMap) {
 		List<String> list = new ArrayList();
 
-		bizMap.put("merId", "QRC331090000042");
+		bizMap.put("merId", "11");
 		bizMap.put("applyNum", "0");
 		bizMap.put("applyNameList", list.toString());
 	}
 
 	// 4.2.5图片上传
 	protected static void image(Map<String, Object> bizMap) {
-		File file = new File("C:\\21028119831105153x.jpg");
+		File file = new File("11");
 		bizMap.put("imgContent", file.toString());
-		bizMap.put("merId", "QRC370200002775");
+		bizMap.put("merId", "11");
 		bizMap.put("imgType", "02");
 	}
 
 	protected static void normalParam(Map<String, Object> paramMap) {
 		paramMap.put("version", "1.0");
-		paramMap.put("expandcode", "C0001591");
+		paramMap.put("expandcode", "11");
 		paramMap.put("encoding", "UTF-8");
 		paramMap.put("requestId", "11");
-		paramMap.put("signMethod", "RSA2");
+		paramMap.put("signMethod", "11");
 		paramMap.put("signature", "sss");
 	}
 
