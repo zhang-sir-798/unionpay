ecedaa025f57d8b4a0d5a8437114c5b93eb5c02e
zhangxu <352521799@163.com>
2025-08-08 19:53:47 +0800

    #0004:clean useless log.

--- unionpay/src/main/java/com/dx/util/dbconfig/DataSourceAspect.java
+++ unionpay/src/main/java/com/dx/util/dbconfig/DataSourceAspect.java
@@ -2,28 +2,25 @@ package com.dx.util.dbconfig;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.reflect.MethodSignature;
+
 import java.lang.reflect.Method;
 
-public class DataSourceAspect {  
-    public void before(JoinPoint point)  
-    {  
-        Object target = point.getTarget();  
-        //System.out.println(target.toString());  
-        String method = point.getSignature().getName();  
-        //System.out.println(method);  
-        Class<?> classz = target.getClass();  
-        Class<?>[] parameterTypes = ((MethodSignature) point.getSignature())  
-                .getMethod().getParameterTypes();  
-        try {  
-            Method m = classz.getMethod(method, parameterTypes);  
-            //System.out.println(m.getName());  
-            if (m != null && m.isAnnotationPresent(DataSource.class)) {  
-                DataSource data = m.getAnnotation(DataSource.class);  
-                DynamicDataSourceHolder.putDataSource(data.value());  
-            }  
+public class DataSourceAspect {
+    public void before(JoinPoint point) {
+        Object target = point.getTarget();
+        String method = point.getSignature().getName();
+        Class<?> classz = target.getClass();
+        Class<?>[] parameterTypes = ((MethodSignature) point.getSignature())
+                .getMethod().getParameterTypes();
+        try {
+            Method m = classz.getMethod(method, parameterTypes);
+            if (m != null && m.isAnnotationPresent(DataSource.class)) {
+                DataSource data = m.getAnnotation(DataSource.class);
+                DynamicDataSourceHolder.putDataSource(data.value());
+            }
 
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        }  
-    }  
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }  
